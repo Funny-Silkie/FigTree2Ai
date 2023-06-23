9462ac33806d01c0e450b19f6bb9a9cff850f3c2
Funny_Silkie <ban.grumble99rar@gmail.com>
2023-06-23 14:02:35 +0900

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 readonly BUILD_DIR="build"
+readonly TMP_PATH="tmp"
 
 cd $(dirname $0)
 cd ..
+
+readonly LICENSE_PATH=$(pwd)/LICENSE.md
 
 # Transpile to JavaScript
 npx tsc
@@ -11,5 +14,16 @@ npx tsc
 # Edit JS files
 cd $BUILD_DIR
 for FILE in $(ls *.js); do
-    sed -e "s/@ts-ignore//g" -i $FILE
+    echo $FILE
+    # remove "@ts-ignore" (causes syntax error)
+    sed -e "s/@ts-ignore//g" $FILE > $TMP_PATH
+    # print script info
+    echo "// ${FILE} in FigTree2Ai" > $FILE
+    echo "// Version: 1.1.0" >> $FILE
+    # print license
+    while read LINE; do
+        echo "// ${LINE}" >> $FILE
+    done < $LICENSE_PATH
+    cat $TMP_PATH >> $FILE
+    rm $TMP_PATH
 done
